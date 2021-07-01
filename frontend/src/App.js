f0f8c07c5da72610584eb8367d9adaff6e8bebd5
dianaGapac <gapacdiana@gmail.com>
2021-07-01 14:22:14 +0800

    Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import {Container} from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
     <div className="App">
       <Header/>
       <main>
         <Container>
-        <h1> WELCOME TO MARKETSHOP</h1>
+            <HomeScreen/>
         </Container>
       </main>
      
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'react-bootstrap';
+
+
+const Product = ({product}) => {
+    return (
+        <Card className='my-3 p-3 rounded'>
+            <a href= {`/products/${product._id}`}></a>
+            <Card.Img src= {product.image} variant= 'top' /> 
+        </Card>
+    )
+}
+
+export default Product
