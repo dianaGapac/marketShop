1c6b918f926971fc9272293c5cd55cc42d31be57
dianaGapac <gapacdiana@gmail.com>
2021-07-01 13:36:22 +0800

    3rd Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Nike Air Huarache',
+        image: '/images/air-huarache.jpg',
+        description: 'High Quality Shoes in your Area.',
+        brand: 'Nike',
+        category: 'Shoes',
+        price: 200,
+        countInstock: 5,
+        rating: 5,
+        numReviews: 5,
+    },
+    {
+        _id: '1',
+        name: 'Nike Air Huarache',
+        image: '/images/air-huarache.jpg',
+        description: 'High Quality Shoes in your Area.',
+        brand: 'Nike',
+        category: 'Shoes',
+        price: 200,
+        countInstock: 5,
+        rating: 5,
+        numReviews: 5,
+    },
+    },
+
+
+]
