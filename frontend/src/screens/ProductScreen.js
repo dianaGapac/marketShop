878168204aac74970bf239c12d402c2ff989d8f6
dianaGapac <gapacdiana@gmail.com>
2021-07-14 12:55:38 +0800

    Added qty Button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
-import React,{ useEffect } from 'react';
+import React,{ useState,useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import {Row,Col,Image,ListGroup,Button} from 'react-bootstrap';
+import {Row,Col,Image,ListGroup,Button,Form} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useDispatch,useSelector} from 'react-redux'
 import {listProductDetails} from '../actions/productActions'
@@ -9,6 +9,8 @@ import Loader from '../components/Loader'
 //import Variations from '../components/Variations'
 
 const ProductScreen = (props) => {
+
+    const [qty, setQty] = useState(0)
     //product state
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
@@ -47,14 +49,55 @@ const ProductScreen = (props) => {
                         <ListGroup variant='flush'>
                            
                            <ListGroup.Item>
-                               <h4> {`Price: ${product.price}`} </h4> 
+                               <h4> {`Price: $${product.price}`} </h4> 
                                
                         {/* <Variations product = {product}>  </Variations> */} 
 
                            </ListGroup.Item>
                            <ListGroup.Item>
-                            <h5>Status: {product.countInStock>0 ? 'Available' : 'Unavailable' }</h5>
+                            <span>Status: {product.countInStock>0 ? 'Available' : 'Unavailable' }</span>
                            </ListGroup.Item>
+                           { product.countInStock > 0? (
+                                    <ListGroup.Item>
+                                       {/*  <Form>
+                                            <Button className='btn-block btn-success'> <i className = 'fa fa-minus'> </i></Button>
+                                                <input type='number' className='p-3 border border-succes'> {qty} </input>
+                                            <Button className='btn-block btn-success'> <i className = 'fa fa-plus'> </i></Button>
+                                        </Form> */}
+                                        <Form.Group  >
+                                        <Row >
+                                            <Col md={1}>
+                                            <Button className='btn-block btn-success mx-1'
+                                            onClick = {(e) => setQty(qty>0? qty-1 : qty)}> 
+                                            <i className = 'fa fa-minus' > </i></Button>
+                                            </Col>
+                                            <Col md={1}>
+                                            <Form.Control 
+                                            className="border border-success mx-1"
+                                            style ={{width: '48px'}}
+                                            type="text" 
+                                            placeholder={qty} />
+                                            </Col>
+                                            <Col > 
+                                            <Button className='btn-block btn-success mx-2'
+                                            onClick = {(e) => setQty(qty+1)}
+                                            > 
+                                            <i className = 'fa fa-plus'> </i></Button>
+                                            </Col>
+
+                                          
+
+                                        </Row>
+                                   
+
+                                        </Form.Group>
+
+                                    </ListGroup.Item> 
+                           ) : '...'
+                        
+                      
+                           }
+
                            <ListGroup.Item>
                                <Row>
                                 <Button 
