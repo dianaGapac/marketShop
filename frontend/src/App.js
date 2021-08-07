39937d037afc302d38558840dae25c68cf96ba4a
dianaGapac <gapacdiana@gmail.com>
2021-08-07 12:40:52 +0800

    User LogIn Screen & Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import {Container} from 'react-bootstrap';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Container} from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from'./screens/LoginScreen'
 
 
 function App() {
@@ -16,6 +17,8 @@ function App() {
             <Route path='/' component={HomeScreen} exact/>
             <Route path='/product/:id' component={ProductScreen}/>
             <Route path='/cart/:id' component= {CartScreen}/>
+
+            <Route path='/login/' component={LoginScreen}/>
             
 
            </Container>
