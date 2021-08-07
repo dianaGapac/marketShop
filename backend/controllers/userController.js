0cb1c32a9390e109b4ed7bd37e58b1103f9890c3
dianaGapac <gapacdiana@gmail.com>
2021-08-07 10:27:49 +0800

    User Reg & Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,43 @@ const authUser = asyncHandler (async( req,res) =>{
 }) 
 
 
+// @desc REGISTER NEW USER
+// @route POST '/api/users'
+// @access Public
+
+const registerUser = asyncHandler (async( req,res) =>{
+    const {name,email, password} = req.body
+    
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User Already Exists')
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('User Not Found')
+    }
+
+
+
+}) 
+
+
 
 
 // @desc  get User profile 
@@ -50,5 +87,7 @@ const getUserProfile = asyncHandler (async( req,res) =>{
 
 export {
     authUser,
-    getUserProfile
+    registerUser,
+    getUserProfile,
+
 }
