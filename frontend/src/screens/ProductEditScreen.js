04b7f2b92e5de8da66b1af1ef17110857dbabb97
dianaGapac <gapacdiana@gmail.com>
2021-09-24 13:50:42 +0800

    Imge Upload Front End

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import xios from 'axios'
 import {Link} from 'react-router-dom'
 import {Form, Button} from 'react-bootstrap'
 import { useDispatch, useSelector} from 'react-redux'
@@ -7,6 +8,7 @@ import Loader from '../components/Loader'
 import FormContainer  from '../components/FormContainer'
 import {listProductDetails, updateProduct} from '../actions/productActions' 
 import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
+import axios from 'axios'
 
 
 
@@ -22,9 +24,10 @@ const ProductEditScreen = ({match,history}) => {
     const [category, setCategory] = useState('')
     const [countInStock,setCountInStock] = useState(0)
     const [description,setDescription] = useState('')
+    const [uploading,setUploading] = useState(false)
     
 
-    const productDetails = useSelector(state=> state.productDetails)
+    const productDetails  = useSelector(state=> state.productDetails)
     const {loading, error, product} = productDetails
 
     const productUpdate = useSelector(state=> state.productUpdate)
@@ -49,6 +52,31 @@ const ProductEditScreen = ({match,history}) => {
             description
 
       }))
+    }
+    const uploadFileHandler = async(e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+
+        formData.append('image', file)
+        setUploading(true)
+        
+        try {
+            const config = {
+                header: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+
+            const {data} = await axios.post ('/api/upload', formData, config)
+            setImage(data)
+            setUploading(false)
+        } catch (error) {
+
+            console.error(error)
+            setUploading(false)
+
+            
+        }
     }
 
     useEffect (()=>{
@@ -102,13 +130,6 @@ const ProductEditScreen = ({match,history}) => {
                         <Form.Control type='text' placeholder={name} value= {name} onChange={(e) => setName(e.target.value)}>  
                         </Form.Control>
                     </Form.Group>
-    
-               
-                    <Form.Group controlId='price'> 
-                        <Form.Label> Price</Form.Label>
-                        <Form.Control type='number' placeholder={price} value= {price} onChange={(e) => setPrice(e.target.value)}>  
-                        </Form.Control>
-                    </Form.Group>
 
                     
                     <Form.Group controlId='price'> 
@@ -122,7 +143,13 @@ const ProductEditScreen = ({match,history}) => {
                         <Form.Label> Image</Form.Label>
                         <Form.Control type='text' placeholder={image} value= {image} onChange={(e) => setImage(e.target.value)}>  
                         </Form.Control>
+
+                        <Form.File id='image-file' label='Choose File' custom onChange={uploadFileHandler}></Form.File>
+                        {uploading && <Loader />}
+
+
                     </Form.Group>
+
 
                     
                     <Form.Group controlId='brand'> 
