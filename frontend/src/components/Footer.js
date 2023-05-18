b29062e14ef127ed0e71bd48cc3e24c90277042f
dianaGapac <74040969+dianaGapac@users.noreply.github.com>
2023-05-18 22:04:06 +0800

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className='text-center py-3' >
-                        COPYRIGHT &copy; |S|NEAKY
+                        COPYRIGHT &copy; |S|NEAKY 2021
                     </Col>
                 </Row>
             </Container>
